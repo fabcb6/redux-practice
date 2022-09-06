d9e1ea1be88cb27d765e851b9370625eee69b742
Fabian Campos <fabcb6@gmail.com>
2022-09-06 00:35:06 -0600

    practice - react - redux-toolkit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          Test
+        </a>
+      </div>
+    </nav>
+  );
+};
